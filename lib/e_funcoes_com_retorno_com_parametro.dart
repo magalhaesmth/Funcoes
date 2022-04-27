892fb2147ddaef6d28e6f8f7cdd926b95af65f8d
Matheus Magalhães Urbano <89428438+magalhaesmth@users.noreply.github.com>
2022-04-26 22:39:03 -0300

    Update e_funcoes_com_retorno_com_parametro.dart

--- lib/e_funcoes_com_retorno_com_parametro.dart
+++ lib/e_funcoes_com_retorno_com_parametro.dart
@@ -1,9 +1,19 @@
-void show() {
+/*void show() {
   var resultado = verificarAprovacao(6, 5);
   print("O aluno esta $resultado");
 }
 
 String verificarAprovacao(double nota1, double nota2) {
   double media = (nota1 + nota2) / 2;
-  return (media >= 6) ? "Aprovado" : "Reprovado";
+  return (media >= 6) ? "Aprovado" : "Reprovado";*/
+}
+
+void show() {
+  var restante = verificarAprovacao(1200, 700);
+  print("O saldo é de $restante");
+}
+
+String verificarSaldo(double salario, double debitos) {
+  double restante = (salario - debitos);
+  return (restante < salario) ? "Saldo Positivo" : "Saldo Negativo";
 }
